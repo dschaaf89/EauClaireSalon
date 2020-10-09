caa25791fe3e2e0d2c0f9d760df5af6276b1a678
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-10-09 10:47:49 -0700

    finished the Client controller and Stylist controller

--- HairSalon/Controllers/ClientController.cs
+++ HairSalon/Controllers/ClientController.cs
@@ -6,19 +6,19 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using HairSalon.Models;
 namespace HairSalon.Controllers
 {
-    public class ClientController : Controller
+  public class ClientController : Controller
+  {
+    private readonly HairSalonContext _db;
+    public ClientController(HairSalonContext db)
     {
-        private readonly HairSalonContext _db;
-        public ClientController(HairSalonContext db)
-        {
-            _db = db;
-        }
-        public ActionResult Index()
-    {
-        List<Client> model = _db.Clients.Include(Client => Client.Stylist).ToList();
-        return View(model);
+      _db = db;
     }
-        public ActionResult Create()
+    public ActionResult Index()
+    {
+      List<Client> model = _db.Clients.Include(Client => Client.Stylist).ToList();
+      return View(model);
+    }
+    public ActionResult Create()
     {
       ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "StylistName");
       return View();
@@ -26,16 +26,49 @@ namespace HairSalon.Controllers
     [HttpPost]
     public ActionResult Create(Client client)
     {
-      
+
       _db.Clients.Add(client);
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
     public ActionResult Details(int id)
     {
-        Client thisClient = _db.Clients.FirstOrDefault(Clients => Clients.ClientId == id);
-        ViewBag.StylistName = _db.Stylists.FirstOrDefault(stylists => stylists.StylistId == thisClient.StylistId);
+      Client thisClient = _db.Clients.FirstOrDefault(Clients => Clients.ClientId == id);
+      ViewBag.StylistName = _db.Stylists.FirstOrDefault(stylists => stylists.StylistId == thisClient.StylistId);
+      return View(thisClient);
     }
+    public ActionResult Edit(int id)
+    {
+      var thisClient = _db.Clients.FirstOrDefault(Client => Client.ClientId == id);
+      return View(thisClient);
     }
-    
+    [HttpPost]
+    public ActionResult Edit(Client client)
+    {
+      _db.Entry(client).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    public ActionResult Delete(int id)
+    {
+      var thisClient = _db.Clients.FirstOrDefault(Client => Client.ClientId == id);
+      return View(thisClient);
+    }
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisClient = _db.Clients.FirstOrDefault(Client => Client.ClientId == id);
+      _db.Clients.Remove(thisClient);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    [HttpPost]
+    public ActionResult Search(string search)
+    {
+      List<Client> searchList = _db.Clients.Include(Client => Client.Stylist).ToList();
+      List<Client> model = Client.Search(searchList, search);
+      return View(model);
+    }
+  }
+
 }
--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -1,3 +1,8 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 namespace HairSalon.Models
 {
     public class Client
@@ -6,5 +11,22 @@ namespace HairSalon.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public virtual Stylist Stylist { get; set; }
+        public int StylistId { get; set; }
+
+    public static List<Client> Search(List<Client> allClients, string searchParam)
+    {
+      List<Client> matchingClients = new List<Client> { };
+      if (searchParam != null)
+      {
+        foreach (Client client in allClients)
+        {
+          if (client.Name.ToUpper().Contains(searchParam.ToUpper()))
+          {
+            matchingClients.Add(client);
+          }
+        }
+      }
+      return matchingClients;
+    }
     }
 }
